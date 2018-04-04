d686de6c8e55ed67b45127ca9178789c714829f0
Canop <cano.petrole@gmail.com>
2018-04-04 22:20:19 +0200

    first version

--- README.md
+++ README.md
@@ -1,2 +1,53 @@
 # file-archive
-A simple archiver storing files in a date based tree
+
+A simple and fast archiver storing files in a date based directory tree, handling collisions and directory creation.
+
+[![Chat on Miaou](https://dystroy.org/miaou/static/shields/room-en.svg?v=1)](https://dystroy.org/miaou/1?Miaou)
+[![Chat on Miaou](https://dystroy.org/miaou/static/shields/room-fr.svg?v=1)](https://dystroy.org/miaou/3?Code_Croissants)
+
+## Usage
+
+### Preparing the archive
+
+Initialize your instance of `FileArchiver`:
+
+	const FileArchive = require("file-archive")
+	const archive = new FileArchive("/var/stuff", "map-", "json")
+
+This operation makes no change on disk.
+
+### Adding to the archive
+
+	let filepath = await archive.write(buffer) // you can pass byte buffers or strings
+
+This writes a new file and gives you its filepath, for example
+
+	"/var/stuff/2018/04/03/map-20:01:05:667.json"
+
+If you're calling this function several times in the same millisecond, the collision will be catched and different files will be written (by incrementing the milliseconds part of the name).
+
+Having several FileArchivers mapped to the same directory is totally OK. It lets you store several types of files in the same directory hierarchy.
+
+It's possible to pass a specific time (instead of having the current one used):
+
+	let filepath = await archive.write(buffer, someTimeMillis)
+
+### Finding the last entry
+
+You can easily get the last entry for a year, a month, a day, or in the archive:
+
+	let filepath = await archive.last(2018, 4)
+	let filepath = await archive.last("2018", "04")
+	let filepath = await archive.last(2018, 4, 3)
+	let filepath = await archive.last()
+
+This function returns `undefined` when there's nothing for that specific period.
+
+### Listing
+
+You can list the years in the archive, the months in a year, the days in a month, or the entries in a day:
+
+	let years = await archive.list()
+	let monthsOf2018 = await archive.list(2018)
+	let daysOfMay = await archive.list("2018", "05")
+	let entriesThatSpecificDay = await archive.list(2018, 5, 7)
---
+++ index.js
@@ -0,0 +1,141 @@
+
+const fs = require("fs")
+const path = require("path")
+
+// return a promise, fail with "EEXIST" when the file already exists
+function writeNewAsync(filepath, data){
+	return new Promise((resolve, reject)=>{
+		fs.writeFile(filepath, data ,{ flag: "wx" }, function(err){
+			if (!err) resolve(true)
+			else reject(err)
+		})
+	})
+}
+
+// simple promisification of fs.readdir
+function readdirAsync(filepath){
+	return new Promise((resolve, reject)=>{
+		fs.readdir(filepath, (err, items)=>{
+			if (err) reject(err)
+			else resolve(items)
+		})
+	})
+}
+
+function lp(number, size=2){
+	return String(number).padStart(size, "0")
+}
+
+class FileArchiver{
+	constructor(basepath, basename, extension){
+		this.basepath = path.normalize(basepath)
+		this.basename = basename;
+		this.extension = extension;
+	}
+	dateDirectory(date){
+		return path.join(
+			this.basepath,
+			`${date.getUTCFullYear()}/${lp(date.getUTCMonth()+1)}/${lp(date.getUTCDate())}`
+		)
+	}
+	filepath(date){
+		const hour = lp(date.getUTCHours())
+		const min = lp(date.getUTCMinutes())
+		const sec = lp(date.getUTCSeconds())
+		const ms = lp(date.getUTCMilliseconds(), 3)
+		return path.join(
+			this.dateDirectory(date),
+			`${this.basename}${hour}:${min}:${sec}:${ms}.${this.extension}`
+		)
+	}
+	async write(data, time){
+		if (!time) time = Date.now()
+		let madeDirs = false
+		for(;;) {
+			let date = new Date(time)
+			let filepath = this.filepath(new Date(time))
+			try {
+				await writeNewAsync(filepath, data)
+				return filepath
+			} catch (e) {
+				if (e.code==="ENOENT" && !madeDirs) {
+					madeDirs = true
+					this.mkdirs(date)
+				} else if (e.code==="EEXIST") {
+					time++;
+					madeDirs = false // for when incrementing the time changes the day
+				} else {
+					throw e
+				}
+			}
+		}
+	}
+	filteredNames(items){
+		return items.filter(filename=>this.filterName(filename))
+	}
+	filterName(filename){
+		return filename.startsWith(this.basename) && filename.endsWith("."+this.extension)
+	}
+	async last(...parts){
+		let filepath = this.basepath
+		try {
+			for (let i=0; i<=3; i++) {
+				if (parts[i]===undefined) {
+					let items = await readdirAsync(filepath)
+					if (i===3) {
+						for (let j=items.length; j--;) {
+							if (this.filterName(items[j])) {
+								return items[j]
+							}
+						}
+						return
+					}
+					parts[i] = items[items.length-1]
+				} else if (i && typeof parts[i]==="number") {
+					parts[i] = lp(parts[i])
+				}
+				filepath += path.sep + parts[i]
+			}
+		} catch (e) {
+			if (e.code==="ENOENT") return // nothing on that day
+			throw e
+		}
+		return filepath
+	}
+	async list(...parts){
+		let filepath = this.basepath
+		try {
+			for (let i=0; i<=3; i++) {
+				if (parts[i]===undefined) {
+					let items = await readdirAsync(filepath)
+					if (!items) return []
+					if (i===3) {
+						items = this.filteredNames(items)
+					}
+					return items
+				} else if (i && typeof parts[i]==="number") {
+					parts[i] = lp(parts[i])
+				}
+				filepath += path.sep + parts[i]
+			}
+		} catch (e) {
+			if (e.code==="ENOENT") return []
+			throw e
+		}
+	}
+	mkdirs(date){
+		if (!date) date = new Date
+		let filepath = this.dateDirectory(date)
+		if (fs.existsSync(filepath)) return this
+		const tokens = filepath.split(path.sep)
+		filepath = ""
+		for (let i=0; i<tokens.length; i++) {
+			filepath += tokens[i] + path.sep
+			if (fs.existsSync(filepath)) continue
+			fs.mkdirSync(filepath)
+		}
+		return this
+	}
+}
+
+module.exports = FileArchiver
